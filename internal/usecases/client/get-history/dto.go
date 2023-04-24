d4d345365179b26bbb3e080c031cd855388a16d3
Evgeniy Krivenko <evgeniy.krivenko.work@gmail.con>
2023-04-24 14:06:56 +0300

    feat: 3.3.8 impl getHistory useCase

---
+++ internal/usecases/client/get-history/dto.go
@@ -0,0 +1,58 @@
+package gethistory
+
+import (
+	"errors"
+	"time"
+
+	messagesrepo "github.com/evgeniy-krivenko/chat-service/internal/repositories/messages"
+	"github.com/evgeniy-krivenko/chat-service/internal/types"
+	"github.com/evgeniy-krivenko/chat-service/internal/validator"
+)
+
+type Request struct {
+	ID       types.RequestID `validate:"required"`
+	ClientID types.UserID    `validate:"required"`
+	PageSize int             `validate:"omitempty,gte=10,lte=100"`
+	Cursor   string          `validate:"omitempty,base64url"`
+}
+
+func (r Request) Validate() error {
+	isValidPage := r.PageSize >= 10 && r.PageSize <= 100
+
+	if r.Cursor == "" && r.PageSize == 0 {
+		return errors.New("neither cursor nor pagesize specified")
+	}
+	if r.Cursor != "" && isValidPage {
+		return errors.New("cursor and pagesize specified")
+	}
+	return validator.Validator.Struct(r)
+}
+
+type Response struct {
+	Messages   []Message
+	NextCursor string
+}
+
+type Message struct {
+	ID                  types.MessageID
+	AuthorID            types.UserID
+	Body                string
+	CreatedAt           time.Time
+	IsBlocked           bool
+	IsVisibleForManager bool
+	IsReceived          bool
+	IsService           bool
+}
+
+func adaptMessage(m messagesrepo.Message) Message {
+	return Message{
+		ID:                  m.ID,
+		AuthorID:            m.AuthorID,
+		Body:                m.Body,
+		CreatedAt:           m.CreatedAt,
+		IsBlocked:           m.IsBlocked,
+		IsVisibleForManager: false,
+		IsReceived:          m.IsVisibleForManager && !m.IsBlocked,
+		IsService:           m.IsService,
+	}
+}
